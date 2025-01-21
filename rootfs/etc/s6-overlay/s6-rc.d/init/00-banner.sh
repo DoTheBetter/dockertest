dc35e332d250a1373cdab3586195b6258b7de318
DTB <183888664+DoTheBetter@users.noreply.github.com>
2025-01-21 20:48:27 +0800

    8

--- rootfs/etc/s6-overlay/s6-rc.d/init/00-banner.sh
+++ rootfs/etc/s6-overlay/s6-rc.d/init/00-banner.sh
@@ -1,13 +1,13 @@
 #!/command/with-contenv sh
 
 echo "==========================================="
-echo
+echo " "
 cat /etc/s6-overlay/s6-rc.d/init/00-banner
-echo
+echo " "
 echo "Rsync 版本: $(rsync --version | sed -n 's/.*\([0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/p')"
-echo
+echo " "
 echo "SSH服务：$([ "$SSH" = "true" ] && echo "已启用" || echo "未启用") | 计划任务：$([ "$CRON" = "true" ] && echo "已启用" || echo "未启用")"
-echo
+echo " "
 echo "Rsync守护进程：$([ "$RSYNC" = "true" ] && echo "已启用" || echo "未启用") | Lsyncd守护进程：$([ "$LSYNCD" = "true" ] && echo "已启用" || echo "未启用")"
-echo
+echo " "
 echo "==========================================="
