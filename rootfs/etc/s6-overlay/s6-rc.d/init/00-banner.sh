d4366d911d1718f02fa82b067ff53cc88e33461f
DTB <183888664+DoTheBetter@users.noreply.github.com>
2025-01-21 20:39:25 +0800

    8

--- rootfs/etc/s6-overlay/s6-rc.d/init/00-banner.sh
+++ rootfs/etc/s6-overlay/s6-rc.d/init/00-banner.sh
@@ -1,15 +1,9 @@
 #!/command/with-contenv sh
 
-cat <<EOF
-===========================================
-
-$(cat /etc/s6-overlay/s6-rc.d/init/00-banner)
-
-Rsync 版本: $(rsync --version | sed -n 's/.*\([0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/p')
-
-SSH服务：$([ "$SSH" = "true" ] && echo "已启用" || echo "未启用") | 计划任务：$([ "$CRON" = "true" ] && echo "已启用" || echo "未启用")
-
-Rsync守护进程：$([ "$RSYNC" = "true" ] && echo "已启用" || echo "未启用") | Lsyncd守护进程：$([ "$LSYNCD" = "true" ] && echo "已启用" || echo "未启用")
-
-===========================================
-EOF
+printf "===========================================\n\n"
+cat /etc/s6-overlay/s6-rc.d/init/00-banner
+printf "\n\n"
+printf "Rsync 版本: $(rsync --version | sed -n 's/.*\([0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/p')\n\n"
+printf "SSH服务：$([ "$SSH" = "true" ] && echo "已启用" || echo "未启用") | 计划任务：$([ "$CRON" = "true" ] && echo "已启用" || echo "未启用")\n\n"
+printf "Rsync守护进程：$([ "$RSYNC" = "true" ] && echo "已启用" || echo "未启用") | Lsyncd守护进程：$([ "$LSYNCD" = "true" ] && echo "已启用" || echo "未启用")\n\n"
+printf "===========================================\n"
