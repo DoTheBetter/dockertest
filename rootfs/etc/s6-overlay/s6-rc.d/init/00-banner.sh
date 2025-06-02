be758b4dbdb5f18c23c5efc29a00ddebad5b21be
DTB <183888664+DoTheBetter@users.noreply.github.com>
2025-06-02 19:34:44 +0800

    1

--- rootfs/etc/s6-overlay/s6-rc.d/init/00-banner.sh
+++
@@ -1,8 +0,0 @@
-#!/command/with-contenv sh
-
-echo "==========================================="
-cat /etc/s6-overlay/s6-rc.d/init/00-banner
-echo " "
-echo " "
-echo "IT Tools版本：IT_TOOLS_VER"
-echo "==========================================="
